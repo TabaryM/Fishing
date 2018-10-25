7ad2472b0c645a813a6f43bc31945a5126a46f37
TabaryM <43238044+TabaryM@users.noreply.github.com>
2018-10-25 16:02:10 +0200

    remove grey square from pause

--- src/Game/Manager/InterfaceManager/InterfaceManager.cpp
+++ src/Game/Manager/InterfaceManager/InterfaceManager.cpp
@@ -4,7 +4,6 @@
 InterfaceManager::InterfaceManager(Stage* s, Input* i) : Manager(s, i) {}
 
 void InterfaceManager::create(){
-  objets["Pause"] = new Object(s->getRenderer(), new Surface(100, 100, 125, 125, 125, 255), 100, 100, 1);
   Manager::create();
 }
 
